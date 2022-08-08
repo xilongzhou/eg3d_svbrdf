05a2920e069b35f6a7c45e876e0772566f23ca9e
Zhou <zhouxilong199213@tamu.edu>
2022-08-08 11:22:03 -0500

    add render1

--- eg3d/training/dataset.py
+++ eg3d/training/dataset.py
@@ -362,8 +362,8 @@ class SVBRDFDataset(Dataset):
         # light_pos = np.array([xy[0], xy[1], 1])*4.0
         # return light_pos
 
-        u_1= torch.abs(torch.randn((1), device=device)*0.05).clamp(0,0.5)  # rmax: 0.95 (default)
-        u_2= torch.rand((1), device=device)
+        u_1= torch.abs(torch.randn((1))*0.05).clamp(0,0.1)  # rmax: 0.95 (default)
+        u_2= torch.rand((1))
 
         r = torch.sqrt(u_1)
         theta = 2*np.pi*u_2
@@ -378,6 +378,7 @@ class SVBRDFDataset(Dataset):
         dist = 4.0 ** torch.empty(1).normal_(mean=1.0, std=0.2)
 
         light_pos = rand_light * dist.cuda()
+        print('light pos: ', light_pos)
 
         return light_pos
 
